2485c5c3ea5010be36ce2b901da685586c499a08
Johnny Ji <johnny@johnnyji.com>
2016-07-31 10:50:11 -0700

    updated gitignoreg

---
+++ src/shared/utils/config.js
@@ -0,0 +1,9 @@
+import Immutable from 'immutable';
+
+export const FONTS = Immutable.fromJS([
+  {filename: 'arial', name: 'Arial', src: null},
+  {filename: 'courier', name: 'Courier', src: null},
+  {filename: 'cursive', name: 'Cursive', src: null},
+  {filename: 'monospace', name: 'Monospace', src: null},
+  {filename: 'open_sans', name: 'Open Sans', src: 'https://fonts.googleapis.com/css?family=Open+Sans:400,300,700'}
+]);
